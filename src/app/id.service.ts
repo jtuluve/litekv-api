6e6baa872ed67d56a21b20495d36eeb216c26240
jtuluve <jtuluve@gmail.com>
2025-08-28 18:42:07 +0530

    Removed nanoid and implemented simple id generation using crypto

--- src/app/id.service.ts
+++ src/app/id.service.ts
@@ -2,33 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { App, AppDocument } from 'src/schemas/app.schema';
+import { randomBytes } from 'crypto';
 
 @Injectable()
 export class IdService {
   private readonly chars =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  private nanoid: () => string;
 
   constructor(
     @InjectModel(App.name) private readonly appModel: Model<AppDocument>,
   ) {}
 
-  private async getNanoid() {
-    const { customAlphabet } = await import('nanoid');
-    return customAlphabet(this.chars, 10);
+  private generateRandomId(length: number = 10): string {
+    const bytes = randomBytes(length);
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += this.chars[bytes[i] % this.chars.length];
+    }
+    return result;
   }
 
   async generateId(): Promise<string> {
-    if (!this.nanoid) {
-      this.nanoid = await this.getNanoid();
-    }
     let id: string;
     let exists = true;
     let retries = 0;
 
     // Collision check just in case XD !
     do {
-      id = this.nanoid();
+      id = this.generateRandomId();
       exists = !!(await this.appModel.exists({ appid: id }));
       retries++;
       if (retries > 5) {
