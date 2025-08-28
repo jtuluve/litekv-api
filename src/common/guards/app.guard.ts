50bb44743341985639201766267704cd356cb139
jtuluve <jtuluve@gmail.com>
2025-08-28 10:51:54 +0530

    Added app guard

---
+++ src/common/guards/app.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Request } from 'express';
+import { Model } from 'mongoose';
+import { App, AppDocument } from 'src/schemas/app.schema';
+
+@Injectable()
+export class AppGuard implements CanActivate {
+  constructor(
+    @InjectModel(App.name) private readonly appModel: Model<AppDocument>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest<Request>();
+    const appId = request.params.appid;
+    if (!appId) {
+      throw new ForbiddenException('App ID is required');
+    }
+
+    const appExists = await this.appModel.exists({ appid: appId });
+
+    if (!appExists) {
+      throw new NotFoundException(`App with ID ${appId} not found`);
+    }
+
+    return true;
+  }
+}
