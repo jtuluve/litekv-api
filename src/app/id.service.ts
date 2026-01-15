da5ef57a5be16e57cdfac7099b1ba92c7970a19d
Jnanesh <74163310+jtuluve@users.noreply.github.com>
2026-01-15 16:27:28 +0530

    fix: path fixed

--- src/app/id.service.ts
+++ src/app/id.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { App, AppDocument } from 'src/schemas/app.schema';
+import { App, AppDocument } from '../schemas/app.schema';
 import { randomBytes } from 'crypto';
 
 @Injectable()
