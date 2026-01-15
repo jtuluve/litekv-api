0ad881f2d6c63f42e3d964b876126c2bf48ff8a1
Jnanesh <74163310+jtuluve@users.noreply.github.com>
2026-01-15 16:26:58 +0530

    fix: path fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { App, AppSchema } from 'src/schemas/app.schema';
+import { App, AppSchema } from '../schemas/app.schema';
 import { IdService } from './id.service';
 
 @Module({
