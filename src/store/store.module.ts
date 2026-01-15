874a1b5a9ec26f2b0514c57e7f4a637a30a19bd6
Jnanesh <74163310+jtuluve@users.noreply.github.com>
2026-01-15 16:32:11 +0530

    Update store.module.ts

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { App, AppSchema } from 'src/schemas/app.schema';
+import { App, AppSchema } from '../schemas/app.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: App.name, schema: AppSchema }])],
